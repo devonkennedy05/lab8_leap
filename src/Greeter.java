e27ee557c51d5d0f1b51f0b78ed2e35bbeea32f4
naribn <brianpov347@gmail.com>
2026-08-18 17:45:49 +0000

    Add very casual greeting

--- src/Greeter.java
+++ src/Greeter.java
@@ -2,4 +2,8 @@ public class Greeter {
     public String greet(String name) {
         return "Greatest of days to you, " + name;
     }
+
+    public String veryCasualGreeting(String name){
+        return "Yo, " + name;
+    }
 }
